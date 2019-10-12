9404dfd6db57058b704c430428f82556f20545dd
KrishnaTejaVemuri <vemuri.kteja.cse17@itbhu.ac.in>
2019-10-12 13:05:02 +0530

    1stcommit

---
+++ src/main/java/com/school/MPPS/MppsGovApplication.java
@@ -0,0 +1,13 @@
+package com.school.MPPS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MppsGovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MppsGovApplication.class, args);
+	}
+
+}
